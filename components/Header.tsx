69ef47dcb803ebe37bbddebbcaffdb02726e66b9
NemanjaP83 <necaintruder@gmail.com>
2022-05-04 18:59:23 +0200

    header created

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,57 @@
+import { useEffect, useState } from 'react'
+import { BellIcon, SearchIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+
 const Header = () => {
-  return <div>Header</div>
+  const [scrolled, setScrolled] = useState(false)
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setScrolled(true)
+      } else {
+        setScrolled(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return (
+    <header className={`${scrolled && 'bg-[#141414]'}`}>
+      <div className="flex space-x-2 md:space-x-10">
+        <img
+          src="https://rb.gy/ulxxee"
+          alt="avatar"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+        <ul className="hidden space-x-8 p-6 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="hidden h-6 w-6 sm:inline" />
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt="avatar"
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
+    </header>
+  )
 }
 
 export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
     <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Movie App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>
-        <Header />
-      </header>
+
+      <Header />
+
       <main>
         {/* banner */}
         <section>
